cf7a3e411dd6d580f36ae460fe449a832a07c8f2
Chenry Lee <chenrylee@users.noreply.github.com>
2018-02-10 12:51:44 +0800

    Update Search-Music.ps1
    Fix some bugs.

--- Search-Music.ps1
+++ Search-Music.ps1
@@ -1,5 +1,6 @@
 
-function Search-Music {
+[cmdletbinding()]
+
     param(
         [Parameter(Mandatory=$true)]
         [string]$Keywords
@@ -7,42 +8,40 @@ function Search-Music {
 
     Begin {
         $songlist=@()
-        $songProp = New-Object -TypeName psobject -Property (
-            @{
-                ID="";
-                Name="";
-                Artist="";
-                Album="";
-                Duration=""
-            }
-        )
     }
 
     PROCESS {
         $songs = ((Invoke-WebRequest "http://api.imjad.cn/cloudmusic/?type=search&search_type=1&s=$Keywords").content | ConvertFrom-Json).result.songs
         foreach ($song in $songs) {
+            $songProp = New-Object -TypeName psobject -Property (
+                @{
+                    ID="";
+                    Name="";
+                    Artist="";
+                    Album="";
+                    Duration=""
+                }
+            )
             $id = $song.ID;
             $name = $song.Name;
             $Artist = $song.ar.Name;
             $Album = $song.al.Name;
             $dt = [timespan]::FromSeconds([int]$($song.dt)/1000)
             $Duration = "{0:mm\:ss}" -f $dt
-            # Write-Host "$id`t$name`t$artist`t$album`t$duration"
             $songProp.ID = $id;
             $songProp.Name = $name;
             $songProp.Artist = $Artist;
             $songProp.Album = $Album;
             $songProp.Duration = $Duration
 
-            # Write-Host $songProp
 
             $songlist += $songProp
+
+            Remove-Variable songprop 
         }
     }
 
     END{
         $songlist | Select-Object ID,Name,Duration,Artist,Album | Format-Table -AutoSize
         Remove-Variable songlist
-        Remove-Variable songprop 
     }
-}
